865acde1601169de6146092f30db94358f56cccb
Vladyslav Serhiienko <vserhiienko@MacBook-Pro-Vladyslav.local>
2020-03-08 17:10:47 +0200

    Remove headers and sources from the root folder.

--- lib/main.cpp
+++ lib/main.cpp
@@ -66,7 +66,7 @@ extern "C" {
 
 #define STB_IMAGE_WRITE_STATIC
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "lib/stb_image_write.h"
 
 enum FileType { FileTypeNone, FileTypeBinary, FileTypeText, };
 struct File {
