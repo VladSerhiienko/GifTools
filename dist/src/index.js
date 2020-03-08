d262c89fe8be1cf92b8d2af840db3e98470cc963
Vasyl Romanchak <vasyl.romanchak@gmail.com>
2020-03-09 01:06:21 +0200

    update webpack config

---
+++ dist/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Loader = require("../bin/GifTools");
+var main = function () { return Loader('../bin/GifTools.wasm'); };
+exports.default = main;
+//# sourceMappingURL=index.js.map
