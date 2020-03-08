394b8aaaa6c58a950fd21caa05e834b0cce53d96
Vladyslav Serhiienko <vserhiienko@MacBook-Pro-Vladyslav.local>
2020-03-08 19:59:07 +0200

    Add option to build fastest WASM module.

--- giftools-build-with-local-emsdk.sh
+++ giftools-build-with-local-emsdk.sh
@@ -1,6 +1,9 @@
 
 source dependencies/emsdk/emsdk_env.sh
-cmake -Bbuild_emscripten -H. -DCMAKE_TOOLCHAIN_FILE=$(pwd)/dependencies/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DGIFTOOLS_BUILD_EMSCRIPTEN=TRUE
+cmake -Bbuild_emscripten -H. \
+    -DCMAKE_TOOLCHAIN_FILE=$(pwd)/dependencies/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+    -DGIFTOOLS_BUILD_EMSCRIPTEN=TRUE \
+    -DGIFTOOLS_BUILD_FASTEST_EMSCRIPTEN=TRUE
 emmake make -C build_emscripten
 cp build_emscripten/GifTools.js bin/GifTools.js
 cp build_emscripten/GifTools.wasm bin/GifTools.wasm
