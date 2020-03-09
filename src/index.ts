40763a22d3f6b5d96542970c7d374e4db7d326bb
Vasyl Romanchak <vsromanch@gmail.com>
2020-03-09 15:51:54 +0200

    add jest.config

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,7 @@
-import * as GifToolsModule from '../bin/web_amalgamated/GifTools';
+export default class GitTools {
+    worker = new Worker('./worker');
 
-export default class GifToolsWrapper {
-    module = GifToolsModule();
-    gifBuilderId: any;
-
-    initGif(width: number, height: number, delay: number) {
-        this.gifBuilderId = this.module._gifBuilderInitialize(width, height, delay);
-    }
-
-    addImage(fileBuffer: Uint8Array, width: number, height: number, delay: number) {
-        const id = this.loadImageFromFileAndResize(fileBuffer, width, height);
-        this.module._gifBuilderAddImage(this.gifBuilderId, id, delay);
-    }
-
-    loadImageFromFileAndResize(fileBuffer: Uint8Array, width: number, height: number): any {
-        const bufferId = this.module.bufferFromUint8Array(fileBuffer);
-        console.log('buffer', 'id', bufferId, 'size', this.module._bufferSize(bufferId));
-
-        const imageId = this.module._imageLoadFromBuffer(bufferId);
-        console.log('image',
-            'id',
-            imageId,
-            'w',
-            this.module._imageWidth(imageId),
-            'h',
-            this.module._imageHeight(imageId),
-            'f',
-            this.module._imageFormat(imageId));
-
-        this.module._objectFree(bufferId);
-
-        const smallImageId = this.module._imageResizeOrClone(imageId, width, height);
-        console.log('resized image',
-            'id',
-            smallImageId,
-            'w',
-            this.module._imageWidth(smallImageId),
-            'h',
-            this.module._imageHeight(smallImageId),
-            'f',
-            this.module._imageFormat(smallImageId));
-
-        this.module._objectFree(imageId);
-        return smallImageId;
-    }
-
-    save(): Uint8Array {
-        var gifBufferId = this.module._gifBuilderFinalize(this.gifBuilderId);
-
-        // TODO: cleanup
-        // GifToolsModule._objectFree(smallImageIds[0]);
-        // GifToolsModule._objectFree(smallImageIds[1]);
-        // GifToolsModule._objectFree(smallImageIds[2]);
-        // GifToolsModule._objectFree(smallImageIds[3]);
-    
-        return this.module.bufferToUint8Array(gifBufferId);
+    addImage(data: Uint8Array) {
+        this.worker.postMessage('addImage', [data]);
     }
 }
