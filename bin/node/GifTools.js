cd72e06c757cf19db4d58ab17d702300aa1b1f8f
Vladyslav Serhiienko <vserhiienko@MacBook-Pro-Vladyslav.local>
2020-03-09 12:00:08 +0200

    Track precompiled and testing stuff with LFS.

---
+++ bin/node/GifTools.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c37e500ac073065714e56568c7d919496db734ec716a154f4fc54184a09f879c
+size 46935
---
+++ bin/web_es6/GifTools.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:203c8cdabb97f88c09ea48e287ab984e8672b232ac4b628996984a1d402f94ab
+size 44372
