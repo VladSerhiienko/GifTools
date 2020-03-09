ac06ce8042a149007ed0431983493069264cd55d
Vlad Serhiienko <vlad.serhiienko@gmail.com>
2020-03-09 16:58:37 +0200

    Add dist folder to LFS.

---
+++ dist/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5525fe48e16a54a1a68ae6073db80522775740d31e10dbb73d8c1473956e8b3e
+size 170393
---
+++ dist/src/index.d.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:76c2995c872094e743cb59ae5f792e7812786c678e44f7573da9b39e530653c0
+size 352
