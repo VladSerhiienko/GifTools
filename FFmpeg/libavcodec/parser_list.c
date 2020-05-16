efebdf7548fc2133396770de0c9be1116a412010
Vlad Serhiienko <vlad.serhiienko@gmail.com>
2020-05-16 23:55:51 +0300

    Add missing files.

---
+++ FFmpeg/libavcodec/parser_list.c
@@ -0,0 +1,3 @@
+static const AVCodecParser * const parser_list[] = {
+    &ff_h264_parser,
+    NULL };
---
+++ FFmpeg/libavformat/demuxer_list.c
@@ -0,0 +1,6 @@
+static const AVInputFormat * const demuxer_list[] = {
+    &ff_aac_demuxer,
+    &ff_h264_demuxer,
+    &ff_mov_demuxer,
+    &ff_mpegvideo_demuxer,
+    NULL };
