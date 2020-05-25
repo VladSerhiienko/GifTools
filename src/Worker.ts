f431e7c287791ab70c65b3fa8b7574d9ac6bd37a
Vasyl Romanchak <vsromanch@gmail.com>
2020-05-26 00:56:51 +0300

    use ctx

--- src/Worker.ts
+++ src/Worker.ts
@@ -1,8 +1,15 @@
+// Worker.ts
+const ctx: Worker = self as any;
+
 export function expensive(time : number) {
-    console.log('received', time);
+    console.log('received 1', time);
     return time * time;
 }
 
-addEventListener('message', event => {
-    postMessage(expensive(event.data));
+// Post data to parent thread
+ctx.postMessage({ foo: "foo" });
+
+// Respond to message from parent thread
+ctx.addEventListener("message", (event) => {
+    ctx.postMessage(expensive(event.data));
 });
